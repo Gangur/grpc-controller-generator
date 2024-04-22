b83cb7ad555072c1ec01c9a8218b25d307280e55
GangurDV <GangurDV@live.ru>
2024-04-22 07:23:59 +0200

    Added CancellationToken cancellationToken;

--- GeneratorInfrastructure/Abstractions/ITestService.cs
+++ GeneratorInfrastructure/Abstractions/ITestService.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Abstractions
 {
     public interface ITestService
     {
-         Task<string> TestAsync(string arg1);
+         Task<string> TestAsync(string arg1, CancellationToken cancellationToken);
     }
 }
